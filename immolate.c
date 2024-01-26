42bbd9ab966f4ff7f779c7938b7bf84c8031d6e5
lmesh91 <89877664+MathIsFun0@users.noreply.github.com>
2024-01-25 19:55:22 -0500

    Update card list for 0.9.3

--- immolate.c
+++ immolate.c
@@ -2,7 +2,7 @@
 int main(int argc, char **argv) {
     
     // Print version
-    printf_s("Immolate v0.9.0n.2\n");
+    printf_s("Immolate v0.9.3-pre1\n");
 
     // Handle CLI arguments
     unsigned int platformID = 0;
