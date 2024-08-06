af370dbc7677979e150ead6cb7d99e9614ed50c7
antipatico <antipatico@users.noreply.github.com>
2024-08-06 23:09:36 +0200

    add perkeo observatory filter

--- src/immolate.cpp
+++ src/immolate.cpp
@@ -24,9 +24,30 @@ long filter(Instance inst) {
     return legendaries;
 };
 
+long filter_perkeo_observatory(Instance inst) {
+    if (inst.nextVoucher(1) == Item::Telescope) {
+        inst.activateVoucher(Item::Telescope);
+        if (inst.nextVoucher(2) != Item::Observatory) return 0;
+    } else return 0;
+    int antes[5] = {1, 1, 2, 2, 2};
+    for (int i = 0; i < 5; i++) {
+        Pack pack = packInfo(inst.nextPack(antes[i]));
+        std::vector<Item> packContents;
+        if (pack.type == Item::Arcana_Pack) {
+            packContents = inst.nextArcanaPack(pack.size, antes[i]);
+        } else if (pack.type == Item::Spectral_Pack) {
+            packContents = inst.nextSpectralPack(pack.size, antes[i]);
+        } else continue;
+        for (int x = 0; x < pack.size; x++) {
+            if (packContents[x] == Item::The_Soul && inst.nextJoker(ItemSource::Soul, antes[i], true).joker == Item::Perkeo) return 1;
+        }
+    }
+    return 0;
+}
+
 
 int main() {
-    Search search(filter, "IMMOLATE", 12, 1000000);
+    Search search(filter_perkeo_observatory, "IMMOLATE", 12, 1000000);
     search.search();
     return 1;
 }
